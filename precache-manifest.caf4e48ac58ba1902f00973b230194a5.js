4e04e50b97bdddf7b59b640c5c3ce5f092fc8521
Bisheswar Mahato <42653296+Vishes77@users.noreply.github.com>
2022-01-28 00:32:13 +0530

    Updates

---
+++ precache-manifest.caf4e48ac58ba1902f00973b230194a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7a458edfe9ff10f1957",
+    "url": "/Lights-Out/static/css/main.0e6219ee.chunk.css"
+  },
+  {
+    "revision": "c7a458edfe9ff10f1957",
+    "url": "/Lights-Out/static/js/main.c7a458ed.chunk.js"
+  },
+  {
+    "revision": "a6f3a2216de1c9165847",
+    "url": "/Lights-Out/static/js/1.a6f3a221.chunk.js"
+  },
+  {
+    "revision": "bcedacc98465ac2cb9bb",
+    "url": "/Lights-Out/static/js/runtime~main.bcedacc9.js"
+  },
+  {
+    "revision": "c0d5aa9f02b07f04f42b104d14e99ef6",
+    "url": "/Lights-Out/index.html"
+  }
+];
